d319a51d413656e258a7623523cf6756fbfeb365
Niharj77 <niharj777@gmail.com>
2025-08-22 12:28:02 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from transformers import pipeline
+
+# Simple AI Text Generator
+generator = pipeline("text-generation", model="gpt2")
+
+prompt = "Artificial Intelligence will change the future because"
+result = generator(prompt, max_length=50, num_return_sequences=1)
+
+print("Generated Text:\n", result[0]['generated_text'])
